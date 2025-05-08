6faf7290adeb5e83a0e8d168dc208fc343c01e07
t-hardik <sai.hardik@cardinaltsinc.net>
2025-05-08 14:59:17 +0530

    Task 2: Using the Math Module for Calculations

---
+++ Module 4: Functions & Modules in Python /Task_2.py
@@ -0,0 +1,19 @@
+import math
+
+# Ask the user for a number as input
+num = float(input("Enter a number: "))
+
+# Calculate the square root
+sqrt_result = math.sqrt(num)
+
+# Calculate the natural logarithm (log base e)
+log_result = math.log(num)
+
+# Calculate the sine of the number (in radians)
+sine_result = math.sin(num)
+
+# Display the results
+print(f"\n")
+print(f"Square root: {sqrt_result}")
+print(f"Logarithm: {log_result}")
+print(f"Sine: {sine_result}")
