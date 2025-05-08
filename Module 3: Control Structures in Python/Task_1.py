4251de11234bacf8796ad9ad7188fe9e35d79a18
t-hardik <sai.hardik@cardinaltsinc.net>
2025-05-08 14:47:56 +0530

    Task 1: Check if a Number is Even or Odd

---
+++ Module 3: Control Structures in Python/Task_1.py
@@ -0,0 +1,8 @@
+# Take integer input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd using if-else
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
