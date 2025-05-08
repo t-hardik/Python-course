f597f2b1c80ca94b82973c0e8a02030537e3a922
t-hardik <sai.hardik@cardinaltsinc.net>
2025-05-08 14:19:53 +0530

    Task 1: Perform Basic Mathematical Operations

--- Module 2: Basic Python Concepts/Task_1.py
+++ Module 2: Basic Python Concepts/Task_1.py
@@ -1,1 +1,20 @@
+# Take input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
+# Perform basic mathematical operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+# Handle division safely
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Undefined (cannot divide by zero)"
+
+# Display the results
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: = {multiplication}")
+print(f"Division: {division}")
