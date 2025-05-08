b8f573ce537e5c2209ca474f5dffda0678201e42
t-hardik <sai.hardik@cardinaltsinc.net>
2025-05-08 14:24:12 +0530

    Task 2: Create a Personalized Greeting

--- Module 2: Basic Python Concepts/Task_2.py
+++ Module 2: Basic Python Concepts/Task_2.py
@@ -1,1 +1,9 @@
+# Take the user's first and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 
+# Concatenate to form the full name
+full_name = first_name + " " + last_name
+
+# Print a personalized greeting
+print(f"\nHello, {full_name}! Welcome to the Python Program.")
